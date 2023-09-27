79bc409b37bd7f1992085db8d2f313be26047530
Achilles-Dev <solohag92@gmail.com>
2023-09-27 12:10:25 +0000

    update loading page

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Box, LinearProgress, Stack } from '@mui/joy'
+import { Box, LinearProgress, Stack, Typography } from '@mui/joy'
 
 import Message from './message'
 
@@ -10,14 +10,22 @@ export default function Loading() {
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
+        alignItems: { md: 'center', xs: 'start' },
         px: { md: 10, xs: 2 },
         height: '100vh',
         width: '100%',
       }}
     >
-      <Stack sx={{ textAlign: 'center' }}>
-        <LinearProgress size='lg' thickness={20} color='primary'>
-          Loading ...
+      <Stack sx={{ width: '100%', textAlign: 'center' }}>
+        <LinearProgress
+          size='lg'
+          thickness={30}
+          color='primary'
+          sx={{ minWidth: { md: '500px', xs: '100%' } }}
+        >
+          <Typography level='h4' color='primary'>
+            Loading ...
+          </Typography>
         </LinearProgress>
         <Message />
       </Stack>
