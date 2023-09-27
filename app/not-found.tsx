531f73b186dae35ce721d9002f858e7ef580fdad
Achilles-Dev <solohag92@gmail.com>
2023-09-27 10:30:51 +0000

    Update not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,5 @@
 export default async function Default() {
+  // await new Promise((resolve) => setTimeout(resolve, 5000))
   return (
     <div style={{ padding: '20px', textAlign: 'center' }}>
       <p>Sorry! Page does not exist</p>
