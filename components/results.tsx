eac788407b0ac74b5297fe78b1c0fd2a3cfbbf66
braincee <kwesi938@gmail.com>
2023-09-29 16:49:00 +0000

    Refactor canvas confetti

--- components/results.tsx
+++ components/results.tsx
@@ -1,59 +1,46 @@
 import { Box, Card, Grid, Typography, Stack } from '@mui/joy'
 import truncateEthAddress from 'truncate-eth-address'
-//! canvas-confetti is the right package to use https://github.com/catdad/canvas-confetti
-// import Confetti from 'react-confetti'
-import confetti from 'canvas-confetti'
-import { useState, useEffect } from 'react'
+import Confetti from 'canvas-confetti';
+import { useState, useEffect } from 'react';
 
-interface OutputProps {
-  txCount: number
-  topTransaction: {
-    tx_hash: string | undefined
-  }
-  topMinerPaid: string
-  topMinerTxs: string
-  gasBurned: string
-  uniqueMiners: string[]
-  allMinersUnique: boolean
-}
 
-export default async function Results({
-  output,
-}: {
-  output: OutputProps | undefined
-}) {
-  const defaults = {
-    spread: 360,
-    ticks: 50,
-    gravity: 0,
-    decay: 0.94,
-    startVelocity: 30,
-    shapes: ['star'],
-    colors: ['FFE400', 'FFBD00', 'E89400', 'FFCA6C', 'FDFFB8'],
-  }
+export  default async function Results({ output }: { output: any }) {
+  const [isConfettiActive, setIsConfettiActive] = useState(false);
 
   useEffect(() => {
     if (output) {
-      setTimeout(() => {
-        confetti({
-          ...defaults,
-          particleCount: 40,
-          scalar: 1.2,
-          shapes: ['star'],
-        })
-        confetti({
-          ...defaults,
+      setIsConfettiActive(true);
+      var confettiSettings = {
+        startVelocity: 30,
+        spread: 360,
+        ticks: 60,
+        zIndex: 0,
+      };
+      function createConfetti(originX: number, originY: number) {
+        Confetti({
+          ...confettiSettings,
           particleCount: 50,
-          scalar: 1.75,
-          shapes: ['circle'],
-        })
-      }, 500)
+          origin: { x: originX, y: originY },
+        });
+      }
+      var duration = 15 * 1000;
+      var animationEnd = Date.now() + duration;
+      var interval = setInterval(() => {
+        var timeLeft = animationEnd - Date.now();
+        if (timeLeft <= 0) {
+          clearInterval(interval);
+          setIsConfettiActive(false);
+        }
+        var particleCount = 50 * (timeLeft / duration);
+        createConfetti(0.1, Math.random() - 0.2);
+        createConfetti(0.7, Math.random() - 0.2);
+      }, 250);
     }
-  }, [output])
-
-  console.log(output)
+  }, [output]);
+  
   return (
     <Box>
+       {isConfettiActive && <Confetti />} 
       <Stack sx={{ display: 'flex', alignItems: 'center' }}>
         <video width='200' height='200' autoPlay loop playsInline>
           <source src='/bat-480.mov' type='video/mp4; codecs=hvc1' />
@@ -77,55 +64,52 @@ export default async function Results({
             }}
           >
             <Typography level='title-lg'>
-              Transaction Count: {output?.txCount}
+              Transaction Count: {output.txCount}
             </Typography>
           </Card>
         </Grid>
         <Grid xs={12} md={6} spacing={2}>
+        <Grid>
+          <Card
+            sx={{
+              mt: 2,
+              p: 2,
+              textAlign: 'center',
+              backgroundColor: '#F8F7F9',
+              width: '100%',
+              height: '60px',
+              '@media (max-width: 767px)': {
+                width: '80%',
+                margin: '0 auto',
+              },
+            }}
+          >
+            <Typography level='title-lg'>Gas Used: {output.gasUsed}</Typography>
+          </Card>
           <Grid>
-            <Card
-              sx={{
-                mt: 2,
-                p: 2,
-                textAlign: 'center',
-                backgroundColor: '#F8F7F9',
-                width: '100%',
-                height: '60px',
-                '@media (max-width: 767px)': {
-                  width: '80%',
-                  margin: '0 auto',
-                },
-              }}
-            >
-              <Typography level='title-lg'>
-                Gas Used: {output?.gasBurned}
-              </Typography>
-            </Card>
-            <Grid>
-              <Card
-                sx={{
-                  mt: 2,
-                  p: 2,
-                  textAlign: 'center',
-                  backgroundColor: '#49D49D',
-                  width: '100%',
-                  '@media (max-width: 767px)': {
-                    width: '80%',
-                    margin: '0 auto',
-                    marginTop: '10px',
-                  },
-                }}
-              >
-                <Typography level='title-lg'>
-                  Top Transaction:{' '}
-                  {output?.topTransaction?.tx_hash &&
-                    truncateEthAddress(output.topTransaction?.tx_hash)}
-                </Typography>
-              </Card>
-            </Grid>
-          </Grid>
+          <Card
+            sx={{
+              mt: 2,
+              p: 2,
+              textAlign: 'center',
+              backgroundColor: '#49D49D',
+              width: '100%',
+              '@media (max-width: 767px)': {
+                width: '80%',
+                margin: '0 auto',
+                marginTop: '10px'
+              },
+            }}
+          >
+            <Typography level='title-lg'>
+              Top Transaction:{' '}
+              {truncateEthAddress(output.topTransaction?.tx_hash)}
+            </Typography>
+          </Card>
         </Grid>
-        {output?.allMinersUnique ? (
+        </Grid>
+        </Grid>
+        {output.allMinersUnique ? (
           <>
             <Grid xs={12} md={6}>
               <Card
@@ -144,7 +128,7 @@ export default async function Results({
               >
                 <Typography level='title-lg'>
                   Top Miner (Transactions):{' '}
-                  {truncateEthAddress(output?.topMinerTxs)}
+                  {truncateEthAddress(output.topMinerTxs)}
                 </Typography>
               </Card>
             </Grid>
@@ -164,7 +148,7 @@ export default async function Results({
                 }}
               >
                 <Typography level='title-md'>
-                  Top Miner Paid: {truncateEthAddress(output?.topMinerPaid)}
+                  Top Miner Paid: {truncateEthAddress(output.topMinerPaid)}
                 </Typography>
               </Card>
             </Grid>
@@ -185,9 +169,7 @@ export default async function Results({
               }}
             >
               <Typography level='title-md'>
-                Top Miner Paid:{' '}
-                {output?.topMinerPaid &&
-                  truncateEthAddress(output.topMinerPaid)}
+                Top Miner Paid: {truncateEthAddress(output.topMinerPaid)}
               </Typography>
             </Card>
           </Grid>
